6798579fb987359c1d608e097c1590e718f821c3
Karmvir <jadejakarmvir12@gmail.com>
2024-03-19 17:42:47 +0530

    Added the node packages and key and token authorization

--- index.js
+++ index.js
@@ -37,19 +37,46 @@ app.get("/basicAuth", async(req, res) => {
         password: yourPassword,
       },
     });
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
     res.status(404).send(error.message);
   }
 });
 
 app.get("/apiKey", async(req, res) => {
+  try{
+    const result = await axios.get("https://bored-api.appbrewery.com/filter",{
+
+    params:{
+      score:5,
+      apiKey:yourAPIKey
+    }
+  }); 
+  res.render("index.ejs", { content: JSON.stringify(result.data) });
+  }
+ 
   //TODO 4: Write your code here to hit up the /filter endpoint
   //Filter for all secrets with an embarassment score of 5 or greater
   //HINT: You need to provide a query parameter of apiKey in the request.
+  catch(error) {
+    res.status(404).send(error.message);
+  }
 });
 
 app.get("/bearerToken", async(req, res) => {
+
+  try{
+  const result = await axios.get("https://bored-api.appbrewery.com/secrets/2",{
+
+  auth:{
+      Authorization: `${yourBearerToken}`,
+    }
+  }); 
+  res.render("index.ejs", { content: JSON.stringify(result.data) });
+  }
+  catch(error){
+    res.status(404).send(error.message);
+  }
   //TODO 5: Write your code here to hit up the /secrets/{id} endpoint
   //and get the secret with id of 42
   //HINT: This is how you can use axios to do bearer token auth:
